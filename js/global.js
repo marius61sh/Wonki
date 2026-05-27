524b565b1dc3c055fb4b5ea5e2772bd9ab07c932
marius61sh <marius.golimaz@gmail.com>
2026-05-27 12:52:19 +0300

    feat: add Tawk.to live chat widget

--- js/global.js
+++ js/global.js
@@ -394,3 +394,13 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
 });
+
+/* ─── TAWK.TO LIVE CHAT ─── */
+(function(){
+  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+  s1.async=true;
+  s1.src='https://embed.tawk.to/6a16bdce36ad8d1c38c41b37/1jpkdarp9';
+  s1.charset='UTF-8';
+  s1.setAttribute('crossorigin','*');
+  s0.parentNode.insertBefore(s1,s0);
+})();
